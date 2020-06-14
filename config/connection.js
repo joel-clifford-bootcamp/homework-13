a49502fbf4bffef48d3ccb0d775f8dbb7d0d08aa
Joel Clifford <joel.clifford@icloud.com>
2020-06-14 13:43:35 -0400

    updated heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
 
-const con = process.env.JAWSDB_URL ? 
-mysql.createConnection(process.env.JAWSDB_URL) : 
-mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "bootcamp",
-  database: "burgers_db"
-});
+let con 
+
+
+if(process.env.JAWSDB_URL) { 
+  con = mysql.createConnection(process.env.JAWSDB_URL) 
+} else{
+  con = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bootcamp",
+    database: "burgers_db"
+  });
+}
 
 con.connect(function(err) {
     if (err) {
